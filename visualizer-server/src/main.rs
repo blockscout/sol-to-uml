c0231bfa5685dc4068a42be2c221063e875b5006
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-09-20 14:27:11 +0300

    + tokio main

--- visualizer-server/src/main.rs
+++ visualizer-server/src/main.rs
@@ -1,13 +1,9 @@
-use futures::try_join;
 
-
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
     let http_server = visualizer_server::run::http_server(8050);
     let grpc_server = visualizer_server::run::grpc_server(8051);
-
-    let (_, _) = try_join!(http_server, grpc_server)?;
-
+    let (_, _) = futures::try_join!(http_server, grpc_server)?;
     Ok(())
 }
