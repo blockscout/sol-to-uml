b22f0e03223bd5cb971fa0cf5f5946306b64b72e
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-09-21 14:20:19 +0300

    + tokio spawn for grpc

--- visualizer-server/src/main.rs
+++ visualizer-server/src/main.rs
@@ -4,8 +4,11 @@ use std::sync::Arc;
 async fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
     let service = Arc::new(visualizer_server::SolidityVisualizerService::default());
-    let grpc_server = visualizer_server::run::grpc_server(service.clone(), 8051);
-    let http_server = visualizer_server::run::http_server(service, 8050);
-    futures::try_join!(grpc_server, http_server)?;
+    let http_server = visualizer_server::run::http_server(service.clone(), 8050);
+    let grpc_server = async move {
+        tokio::task::spawn(visualizer_server::run::grpc_server(service, 8051));
+        Ok(())
+    };
+    let (_, _) = futures::try_join!(http_server, grpc_server)?;
     Ok(())
 }
